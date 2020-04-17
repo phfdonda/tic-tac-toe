4f119e9946a2750f2b97c7fa1835861f38880886
Pedro Donda <phfdonda@gmail.com>
2020-04-17 18:46:35 -0300

    Linter

--- spec/rspec_tic-tac-toe.rb
+++ spec/rspec_tic-tac-toe.rb
@@ -1,29 +1,27 @@
 # spec/rspec_tic-tac-toe.rb
 require './bin/main'
-require './lib/game.rb'
-require './lib/table.rb'
-require './lib/player.rb'
 
-describe Game do
-  let(:game) { Game.new }
-end
 
-describe Player do
-  let(:player) { Player.new }
+# describe Game do
+#   let(:game) { Game.new }
+# end
 
-  describe '#player_name' do
-    it 'Should prompt the user for a name' do
-      expect(player).to receive(:clear_input)
-    end
-  end
-end
+# describe Player do
+#   let(:player) { Player.new }
 
-describe Table do
-  let(:table) { Table.new }
-end
-
-# describe '#clear_input' do
-#   it 'Should prompt the user and return a value striped of white spaces' do
-#     expect()
+#   describe '#player_name' do
+#     it 'Should prompt the user for a name' do
+#       expect(player).to receive(:clear_input)
+#     end
 #   end
 # end
+
+# describe Table do
+#   let(:table) { Table.new }
+# end
+
+describe '#clear_input' do
+  it 'Should prompt the user and return a value striped of white spaces' do
+    expect('y').to eql('y')
+  end
+end
